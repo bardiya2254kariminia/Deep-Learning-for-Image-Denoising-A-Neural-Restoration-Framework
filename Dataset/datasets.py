98c0db4bab50d74345e26b5c403c94c35cff0f5b
Bardiya kariminia <radiya2254@gmail.com>
2025-03-11 11:25:35 +0330

    Added he base format of the project

---
+++ Dataset/datasets.py
@@ -0,0 +1,11 @@
+"""
+implementation of several  dataset classes for different models
+presented by bardiya2254kariminia@github.com
+
+"""
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+# implementation of datasets
---
+++ models/networks.py
@@ -0,0 +1,12 @@
+"""
+implementation of several deep networks for calculating the outputs metrics.
+presented by bardiya2254kariminia@github.com
+
+"""
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+import torchsummary
+
+# implementation of the models:
