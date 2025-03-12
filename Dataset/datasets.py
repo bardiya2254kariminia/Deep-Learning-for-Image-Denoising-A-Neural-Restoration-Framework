f4e65d0b0f20e71e791db8fd5f922fe311be58d6
Bardiya2254kariminia <radiya2254@gmail.com>
2025-03-12 21:41:23 +0330

    Added the stl-10 dataset

--- Dataset/datasets.py
+++ Dataset/datasets.py
@@ -9,14 +9,30 @@ so we are going to use the cifar
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
-from torchvision.datasets import CIFAR100
+from torchvision.datasets import CIFAR100 , STL10
 from config.transform_config import Transform_class
 
 # implementation of datasets
-class Noisy_Cifar(Dataset):
+class Noisy_Cifar100(Dataset):
     def __init__(self , noise_std = 0.1):
-        super(self,Noisy_Cifar).__init__()
+        super(self,Noisy_Cifar100).__init__()
         self.cifar = CIFAR100(root="./data", train=True, download=True)
+        self.noise_std = noise_std
+
+    def __len__(self):
+        return len(self.cifar)
+    
+    def __getitem__(self, index):
+        image , label = self.cifar[index]
+        clean_image = Transform_class.get_transform()["cifar100_transform"](image)
+        noisy_image= clean_image + torch.rand_like(clean_image) * self.noise_std
+        noisy_image = Transform_class.get_transform()["cifar100_transform"](noisy_image)
+        return clean_image , noisy_image
+
+class Noisy_STL10(Dataset):
+    def __init__(self , noise_std = 0.1):
+        super(self,Noisy_STL10).__init__()
+        self.cifar = STL10(root="./data", train=True, download=True)
         self.noise_std = noise_std
 
     def __len__(self):
